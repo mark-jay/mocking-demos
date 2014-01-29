ac496c94ed29d892b7c2b7421bc00665b24b66be
magzhan.karasayev <fallen.s4e@gmail.com>
2014-01-29 23:00:49 +0600

    master: New: null-shortcut added

--- src/test/java/JMockitTestSuit2.java
+++ src/test/java/JMockitTestSuit2.java
@@ -10,12 +10,20 @@ public class JMockitTestSuit2 {
         public int doFoo(int a) {
             return a+1;
         }
+
+        public void doFooVoid(Integer a) {
+            System.out.println("fooVoid called with: " + a);
+        }
     }
 
     public static class Dependant {
         private Dependency dep = new Dependency();
         public int doFoo2(int a) {
             return dep.doFoo(dep.doFoo(a));
+        }
+
+        public void doFooVoid2(Integer a) {
+            dep.doFooVoid(a);
         }
     }
 
@@ -32,17 +40,26 @@ public class JMockitTestSuit2 {
         System.out.println("test 1(1): " + d.doFoo2(2));
     }
 
-    // withXyz
+    /** withXyz; there can be added custom matchers */
     @Test
-    public void test_2(@Injectable final Dependency depcy) {
+    public void test_2(@Mocked final Dependency depcy) {
         new NonStrictExpectations() {{
             depcy.doFoo(withEqual(2)); result = 33;
             depcy.doFoo(withEqual(33)); result = 600;
         }};
 
+        System.out.println("test 2: " + new Dependant().doFoo2(2));
+    }
+
+    /** null-matcher  */
+    @Test
+    public void test_3(@Mocked final Dependency depcy) {
+        new NonStrictExpectations() {{
+            depcy.doFooVoid(null);
+        }};
+
         Dependant d = new Dependant();
-        System.out.println("test 7(0): " + d.doFoo2(2));
-        d.dep = depcy;
-        System.out.println("test 7(1): " + d.doFoo2(2));
+        System.out.println("test 3: ");
+        d.doFooVoid2(2);
     }
 }
